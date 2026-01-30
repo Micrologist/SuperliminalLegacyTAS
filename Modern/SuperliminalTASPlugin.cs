4d27eaa8ee48bdf22c6b95950ca65a7f41cbdb2b
Micrologist <7757920+Micrologist@users.noreply.github.com>
2026-01-30 17:41:25 +0100

    add missing modern file

---
+++ Modern/SuperliminalTASPlugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+using SuperliminalTAS.Demo;
+using SuperliminalTAS.Patches;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SuperliminalTAS;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class SuperliminalTASPlugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        TimeManagerPatcher.Patch(Process.GetCurrentProcess());
+        this.gameObject.AddComponent<DemoRecorder>();
+        this.gameObject.AddComponent<DemoHUD>();
+    }
+}
